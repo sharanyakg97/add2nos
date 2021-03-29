2b550b72d56e9d66cd5586a6b10aba76074ddb15
SHARANYA K G <sharanya.kg@brilliompn.onmicrosoft.com>
2021-03-29 04:54:01 +0000

    First Commit=> Added solution files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using arithmeticlibrary.SampleLibrary;
+using System.Diagnostics;
+using SimpleSumCalculator.Models;
+
+namespace SimpleSumCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult About()
+        {
+            Debug.WriteLine("hello");
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+
+        [HttpPost]
+        public ActionResult Index(SumCalculator obj)
+        {
+
+            obj.result = obj.firstNum + obj.secondNum;
+            return View(obj);
+        }
+    }
+};
---
+++ Models/SumCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using arithmeticlibrary.SampleLibrary;
+
+namespace SimpleSumCalculator.Models
+{
+    public class SumCalculator
+    {
+    public int firstNum { get; set; }
+    public int secondNum { get; set; }
+
+    public int result { get; set; }
+    }
+}
